f61df7cccacbda3b0d80848073403750a841ad9d
Austin Chungath <austin.cv@mu-sigma.com>
2014-03-26 18:32:46 +0530

    script for creating new mvn projects

---
+++ mavenProject.sh
@@ -0,0 +1,5 @@
+mvn archetype:generate \
+      -DarchetypeGroupId=org.apache.maven.archetypes \
+      -DarchetypeArtifactId=maven-archetype-quickstart \
+      -DgroupId=com.austincv.hadoopcv \
+      -DartifactId=ReadImage
